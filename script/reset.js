1716807a410c07e1ee3bc512a0f32bd02987f5af
Bono Mudau <bonomudau6@gmail.com>
2025-10-11 18:32:56 +0200

    Update front-end functionality

--- script/reset.js
+++ script/reset.js
@@ -1,5 +1,43 @@
+function sendOTP() {
+  fetch("url", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: document.getElementById("email").value
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      alert("OTP sent to your email!");
+    } else {
+      alert("Error sending OTP");
+    }
+  })
+  .catch(err => console.log("Error:", err));
+}
 
-const email=document.getElementById("email");
-function  send_otp(email){
-    return true;
+function resetPassword() {
+  const otp = document.getElementById("otp").value;
+  const newPassword = document.getElementById("newPassword").value;
+
+  fetch("url", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: document.getElementById("email").value,
+      otp: otp,
+      newPassword: newPassword
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.success) {
+      alert("Password reset successful!");
+      window.location.href = "login.html";
+    } else {
+      alert("Invalid or expired OTP");
+    }
+  })
+  .catch(err=>console.log("Error:", err));
 }
--- script/signup.js
+++ script/signup.js
@@ -10,3 +10,59 @@ checkb.addEventListener("change", function () {
     passwordInput1.type = "password";
   }
 });
+function signup(){
+  let username=document.getElementById("user_name").value;
+  let phone_no=document.getElementById("phone_no").value;
+  let names=document.getElementById("names").value;
+  let password=document.getElementById("password").value;
+  let password1=document.getElementById("password1").value;
+  let email=document.getElementById("email").value;
+    if(password1!=password){
+      alert("Password do not match");
+      return;
+    }
+    let  userdata={
+      "names":names,
+      "email":email,
+      "phonenumber":phone_no,
+      "password":password 
+    };
+    checkuser(username)
+    .then(useraname_availble=>{
+      if(useraname_availble){
+      alert("username is not available, use a different username")
+      return;
+      }
+    fetch("url", {
+        method:"POST",
+        headers:{
+          "Content-type": "application/json"
+        },
+        body:JSON.stringify(userdata)
+      })
+      .then(res=>res.json())
+      .then(ans=>{
+        alert("Account created successfully, proceed to log in");
+        window.location.href = "login.html"; 
+      })
+      .catch(error=>
+        console.log("error creating an account, please try again later",error)
+
+      ); 
+
+    })    
+}
+function checkuser(username){
+  return fetch("url", {
+    method:"POST",
+    headers:{"Content-type":"application/json"},
+    body:JSON.stringify({
+      "username":username
+    })
+  })
+  .then(res=>res.json())
+  .then(data=>{
+    return data.exist;
+  }
+  )
+}
