acdac4db1fae6d42cfb4bc6f0ba0ffed6518219d
hotel-booking-devs <dlaminikgothatso89@gmail.com>
2025-10-13 22:25:07 +0200

    Add files via upload

--- script/login.js
+++ script/login.js
@@ -7,3 +7,36 @@ checkb.addEventListener("change", function () {
     passwordInput.type = "password";
   }
 });
+function login(){
+  verify_details()
+  .then(verify_user=>{
+    if(verify_user){
+      window.location.href="dashboard.html";
+    }
+    else{
+      alert("incorrect login details");
+    }
+  })
+  .catch(error=>{
+    console.log("error has occured, try again later",error);
+  });
+  
+}
+function verify_details(){
+  return fetch("http://localhost:3000/api/login", {
+    method:"POST",
+    headers:{"Content-type":"application/json"},
+    body:JSON.stringify({
+    "username":document.getElementById("user_name").value,
+    "password":document.getElementById("password").value
+    })
+  })
+  .then(res=>res.json())
+  .then(data=>{
+    return data.correct;
+  })
+  .catch(error=>{
+    console.log("error occurred, try agin later",error);
+     return false;
+  });
+}
